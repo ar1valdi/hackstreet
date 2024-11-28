7d040f8a78f95cc8a7f70c7819ff88c451c61f8a
Jakub Nurkiewicz <78982788+NUREKK2003@users.noreply.github.com>
2024-11-28 14:35:56 +0100

    noise for location

--- data_provider/noise/main.py
+++ data_provider/noise/main.py
@@ -1,3 +1,5 @@
+import requests
+import json
 
 # jelitkowo
 j_lat = 54.430374
@@ -11,8 +13,8 @@ j_ymax = 6033513.07241496
 j_x = (j_xmin+j_xmax)/2
 j_y = (j_ymin+j_ymax)/2
 
-print(f"j_x:{j_x}")
-print(f"j_y:{j_y}")
+#print(f"j_x:{j_x}")
+#print(f"j_y:{j_y}")
 
 # warszawska
 w_lat = 54.336183
@@ -25,6 +27,9 @@ w_ymax = 6023033.422667282
 
 w_x = (w_xmin+w_xmax)/2
 w_y = (w_ymin+w_ymax)/2
+
+#print(f"w_x:{w_x}")
+#print(f"w_y:{w_y}")
 
 # rondo przy lotnisku
 r_lat = 54.383479
@@ -45,7 +50,7 @@ d_lon = w_lon - j_lon
 d_x = w_x - j_x
 d_y = w_y - j_y
 
-print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
+#print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
 
 
 # jelitkowo - rondoa
@@ -54,14 +59,14 @@ d_lon = r_lon - j_lon
 d_x = r_x - j_x
 d_y = r_y - j_x
 
-print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
+#print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
 # rondo - warszawskaa
 d_lat = w_lat - r_lat
 d_lon = w_lon - r_lon
 d_x = w_x - r_x
 d_y = w_y - r_x
 
-print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
+#print(f"y/lat:{d_y/d_lat} x/lon:{d_x/d_lon}")
 
 
 def conv_lat_to_y(lat):
@@ -80,4 +85,18 @@ def conv_x_to_lon(x):
     res = (x-6538378.755415827)/64546.27909872957 + 18.591573
     return res
 
-print(f"56 lat: {conv_lat_to_y(56)}")
+#print(f"56 lat: {conv_lat_to_y(56)}")
+
+def get_noise_by_coords(lat,lon):
+    y = conv_lat_to_y(lat)
+    x= conv_lon_to_x(lon)
+    
+    str_link = f'https://geogdansk.pl/server/rest/services/WSrod/Halas_Drogi_LDWN/MapServer/0/query?f=json&geometry=%7B%22xmin%22%3A{x}%2C%22ymin%22%3A{y}%2C%22xmax%22%3A{x}%2C%22ymax%22%3A{y}%7D&outFields=MAXVAL%2CMINVAL&spatialRel=esriSpatialRelIntersects&where=1%3D1&geometryType=esriGeometryEnvelope&inSR=2177&outSR=2177'
+    #print(str_link)
+    result = requests.get(str_link)
+    res_attrs = result.json()['features'][0]['attributes']
+    
+    print(f"lat:{lat} lon:{lon} x:{x} y:{y} attrs: {res_attrs}")
+    return res_attrs
+
+get_noise_by_coords(54.336183,18.595688)
