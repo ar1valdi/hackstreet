d1db9ea39033b8aaaba8a767e07027326d5e800d
Jakub Nurkiewicz <78982788+NUREKK2003@users.noreply.github.com>
2024-11-29 01:11:20 +0100

    noise measures

---
+++ data_provider/example_files/prepare_noise_for_db.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import datetime
+
+light_data = pd.read_csv('out_noise.csv',sep=";")
+
+
+
+df_outlughts = pd.DataFrame(columns=['date','station_id','value'])
+
+df_outlughts['date'] = ['2024-11-26 00:00:00' for i in range(light_data.shape[0])]
+df_outlughts['station_id'] = light_data['id']
+df_outlughts['value'] = (light_data['min_db']+light_data['max_db'])/2
+df_outlughts['min_value'] = light_data['min_db']
+df_outlughts['max_value'] = light_data['max_db']
+df_outlughts['sensing'] = ['poziom hałasu' for i in range(light_data.shape[0])]
+df_outlughts['category'] = ['hałas' for i in range(light_data.shape[0])]
+
+df_outlughts.to_csv('noise_measures.csv',sep=";",index=False,header=False)
+
+df_outstations = pd.DataFrame(columns=['id','latitude','longitude','station_type'])
+
+df_outstations['id'] = light_data['id']
+df_outstations['latitude'] = light_data['lat']
+df_outstations['longitude'] = light_data['lon']
+df_outstations['station_type'] = light_data['station_type']
+
+df_outstations.to_csv('stations.csv',sep=";",index=False,header=False)
+
+
+
+
+
